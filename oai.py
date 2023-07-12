907aaefca193310f7e4d8949b993dc1e49f9cfc1
sagerock <sage@sagerock.com>
2023-07-12 19:36:51 -0400

    Update oai.py

--- oai.py
+++ oai.py
@@ -9,7 +9,7 @@ import openai
 import streamlit as st
 
 # Assign credentials from environment variable or streamlit secrets dict
-openai.api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
+openai.api_key = os.getenv("OPENAI_API_KEY") or st.secrets[API_KEYS]["OPENAI_API_KEY"]
 
 # Suppress openai request/response logging
 # Handle by manually changing the respective APIRequestor methods in the openai package
